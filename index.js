9687df62559651f2dc200b9628125d4c4800e32c
Aiden <amb@aspy.dev>
2022-02-09 12:15:21 -0500

    holyshitthatworked

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 'use strict';
-function freeURL(){
-    let url=document.getElementById("url").value;
-    url.split('.html')
-    url=url[0]+'#:~:text='
-    window.location.replace(url);
-};
+window.onload=init;
+function init(){
+    document.getElementById("textBox").addEventListener('submit',function(event){
+        event.preventDefault();
+        window.location.replace(document.getElementById("url").value.split('.html')[0]+'.html#:~:text=');
+    });
+}
